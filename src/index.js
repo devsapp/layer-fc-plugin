e8ead535c5d2e2e8f8e34cd2a5efdc9464debe09
wangbricks <brickswong@163.com>
2022-04-14 20:21:06 +0800

    feat reset plugin

--- src/index.js
+++ src/index.js
@@ -1,38 +1,69 @@
 const core = require('@serverless-devs/core');
-const { lodash, Logger } = core;
-const Client = require('./lib/client');
-const Layer = require('./lib/layer');
+const { lodash, loadComponent, Logger } = core;
+const logger = new Logger('layer-fc');
 /**
  * Plugin 插件入口
  * @param inputs 组件的入口参数
  * @param args 插件的自定义参数
  * @return inputs
  */
-
 module.exports = async function index(inputs, args) {
-  const { props = {}, credentials = {}, project = { access: '' } } = inputs;
-  // createclient
-  await Client.setFcClient(props.region, credentials, project.access);
-  const layer = new Layer();
+  logger.debug(`inputs params: ${JSON.stringify(inputs)}`);
+  logger.debug(`args params: ${JSON.stringify(args)}`);
+  const { codeUri, name, update = false, runtime, description } = args;
+  /**
+   * handleInputs
+   * props
+   * layerName: 必填
+   * code: 必填
+   * compatibleRuntime: 选填
+   * description: 选填
+   * **/
+  let _inputs = lodash.merge(inputs, {
+    props: {
+      layerName: name,
+      code: codeUri,
+      compatibleRuntime: runtime,
+      description,
+    },
+  });
+  let publishRes = '';
   // handlelayer
-  const { codeUri, name, update = false, runtime } = args;
-  const result = await layer.list({ prefix: name });
+  const layer = await loadComponent('devsapp/fc-layer');
+  const result = await layer.list(_inputs);
   const isExiting = lodash.includes(
     lodash.map(result, (item) => item.layerName),
     name
   );
   if (update || !isExiting) {
-    await layer.publish({
-      layerName: name,
-      code: codeUri,
-      compatibleRuntime: runtime,
-    });
+    publishRes = await layer.publish(_inputs);
   }
-  return lodash.merge(inputs, {
+  /**
+   * output inputs
+   * environmentVariables  key NODE_PATH
+   * layers
+   * **/
+  const environmentVariables = lodash.get(
+    inputs,
+    'props.function.environmentVariables',
+    {}
+  );
+  const inputNodePath = environmentVariables.NODE_PATH;
+  const layerNodePath = '/opt/node_modules:/opt/nodejs/node_modules';
+  environmentVariables.NODE_PATH = inputNodePath
+    ? `${inputNodePath}:${layerNodePath}`
+    : layerNodePath;
+  const layers = lodash.get(inputs, 'props.function.layers', []);
+
+  publishRes && layers.push(publishRes);
+
+  _inputs = lodash.merge(_inputs, {
     props: {
       function: {
-        runtime: 'custom',
+        environmentVariables,
+        layers,
       },
     },
   });
+  return _inputs;
 };
